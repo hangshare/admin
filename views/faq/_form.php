ce42e3beb9fa8cd9a0ec4dc0282373df13cf41f4
kaldmax <kaldmax@hotmail.com>
2016-07-15 17:21:23 +0300

    Fix Bug

--- views/faq/_form.php
+++ views/faq/_form.php
@@ -9,22 +9,15 @@ use yii\widgets\ActiveForm;
 ?>
 
 <div class="faq-form">
-
     <?php $form = ActiveForm::begin(); ?>
-
     <?= $form->field($model, 'categoryId')->dropDownList($model::$CategoryStr) ?>
-
     <?= $form->field($model, 'question')->textInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'answer')->textarea(['maxlength' => true]) ?>
-    
     <?= $form->field($model, 'email')->checkbox() ?>
     <?= $form->field($model, 'published')->checkbox() ?>
-
+    <?= $form->field($model, 'lang')->dropDownList(['ar' => 'Arabic', 'en' => 'English'], ['prompt' => '']) ?>
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Add' : 'Save', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
-
     <?php ActiveForm::end(); ?>
-
 </div>
