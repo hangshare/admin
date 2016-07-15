22f1410644fbd281390927848223b1c015c4fe0a
kaldmax <kaldmax@hotmail.com>
2016-07-15 17:21:49 +0300

    Fix Bug

--- views/category/_form.php
+++ views/category/_form.php
@@ -3,6 +3,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use app\models\Category;
+
 /* @var $this yii\web\View */
 /* @var $model app\models\Category */
 /* @var $form yii\widgets\ActiveForm */
@@ -11,6 +12,7 @@ use app\models\Category;
     <?php $form = ActiveForm::begin(); ?>
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
     <?= $form->field($model, 'parent')->dropDownList(ArrayHelper::map(Category::find('parent = 0')->all(), 'id', 'title'), ['prompt' => '']) ?>
+    <?= $form->field($model, 'lang')->dropDownList(['ar' => 'Arabic', 'en' => 'English'], ['prompt' => '']) ?>
     <?= $form->field($model, 'url_link')->textInput(['maxlength' => true]) ?>
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
