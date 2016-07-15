03750b42cbbaadb9a21062a026d93be244549ab2
kaldmax <kaldmax@hotmail.com>
2016-07-15 17:21:37 +0300

    Fix Bug

--- views/emailtemplate/_form.php
+++ views/emailtemplate/_form.php
@@ -9,17 +9,13 @@ use yii\widgets\ActiveForm;
 ?>
 
 <div class="email-template-form">
-
     <?php $form = ActiveForm::begin(); ?>
-
     <?= $form->field($model, 'subject')->textInput(['maxlength' => true]) ?>
-
     <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
-
+    <?= $form->field($model, 'code')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'lang')->dropDownList(['ar' => 'Arabic', 'en' => 'English'], ['prompt' => '']) ?>
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
-
     <?php ActiveForm::end(); ?>
-
 </div>
