d52da2297f8e95626e1640e3ffde6598778701d1
khaled <kaldmax@hotmail.com>
2016-08-08 07:00:58 -0600

    category

--- views/category/index.php
+++ views/category/index.php
@@ -1,7 +1,8 @@
 <?php
-
+use app\models\Category;
 use yii\helpers\Html;
-use yii\grid\GridView;
+use yii\helpers\Url;
+
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\CategorySearch */
@@ -9,6 +10,7 @@ use yii\grid\GridView;
 
 $this->title = 'Categories';
 $this->params['breadcrumbs'][] = $this->title;
+$articlesurl = Yii::t('app', 'articles-url');
 ?>
 <div class="category-index">
     <div class="row">
@@ -16,16 +18,71 @@ $this->params['breadcrumbs'][] = $this->title;
         <p class="pull-right"><?= Html::a('Add Category', ['create'], ['class' => 'btn btn-success']) ?></p>
     </div>
     <div class="row">
-        <?=
-        GridView::widget([
-            'dataProvider' => $dataProvider,
-            'filterModel' => $searchModel,
-            'columns' => [
-                ['class' => 'yii\grid\SerialColumn'],
-                'title',
-                ['class' => 'yii\grid\ActionColumn'],
-            ],
-        ]);
+        <?php
+        $mainMenu = [];
+        foreach ($menu as $menuData) {
+            if ($menuData->parent) {
+                $mainMenu[$menuData->parent]['sub'][] = ['id' => $menuData->id, 'title' => $menuData->title, 'url' => $menuData->url_link,
+                    'lang' => $menuData->lang
+                ];
+            } else {
+                $mainMenu[$menuData->id] = ['id' => $menuData->id, 'title' => $menuData->title, 'url' => $menuData->url_link,
+                    'lang' => $menuData->lang
+                ];
+            }
+        }
         ?>
+        <div class="col-md-6">
+            <ul>
+                <?php foreach ($mainMenu as $mData) : ?>
+                    <?php
+                    if ($mData['lang'] == 'en') : ?>
+                        <li <?php if (isset($mData['sub'])): ?><?php endif; ?>>
+                            <a href="<?= "https://www.hangshare.com/articles/{$mData['url']}" ?>"><?= $mData['title'] ?></a>
+                            <?= Html::a(' (Edit)', ['update','id'=>$mData['id']])?>
+                            <?php if (isset($mData['sub'])): ?>
+                                <ul class="supdropdown">
+                                    <?php foreach ($mData['sub'] as $submenu) : ?>
+                                        <li><?php echo Html::a($submenu['title'], "https://www.hangshare.com/articles/{$mData['url']}/{$submenu['url']}"); ?>
+                                            <?= Html::a(' (Edit)', ['update','id'=>$submenu['id']])?>
+                                        </li>
+                                    <?php endforeach; ?>
+                                </ul>
+                            <?php endif; ?>
+                        </li>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+        <div class="col-md-6">
+            <ul>
+                <?php foreach ($mainMenu as $mData) : ?>
+                    <?php if ($mData['lang'] == 'ar') : ?>
+                        <li <?php if (isset($mData['sub'])): ?><?php endif; ?>>
+                            <a href="<?= "https://www.hangshare.com/مواضيع/{$mData['url']}" ?>"><?= $mData['title'] ?></a>
+                            <?php
+                            //echo
+//                            Html::a('(delete)', 'delete', [
+//                                'data' => [
+//                                    'confirm' => 'Are you sure you want to delete the category?',
+//                                    'method' => 'post',
+//                                ]
+//                            ])
+                            ?>
+                            <?= Html::a(' (Edit)', ['update','id'=>$mData['id']])?>
+                            <?php if (isset($mData['sub'])): ?>
+                                <ul class="supdropdown">
+                                    <?php foreach ($mData['sub'] as $submenu) : ?>
+                                        <li><?php echo Html::a($submenu['title'], "https://www.hangshare.com/مواضيع/{$mData['url']}/{$submenu['url']}"); ?>
+                                            <?= Html::a(' (Edit)', ['update','id'=>$submenu['id']]) ?>
+                                        </li>
+                                    <?php endforeach; ?>
+                                </ul>
+                            <?php endif; ?>
+                        </li>
+                    <?php endif; ?>
+                <?php endforeach; ?>
+            </ul>
+        </div>
     </div>
 </div>
