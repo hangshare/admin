3e0b6075182ffd3e30c8160f7b667a31a8cff96b
khaled <kaldmax@hotmail.com>
2016-08-28 10:55:10 -0600

    category

--- views/user/index.php
+++ views/user/index.php
@@ -31,113 +31,103 @@ $this->params['breadcrumbs'][] = $this->title;
                 'format' => 'raw',
                 'value' => function ($data) {
                     return Html::a($data->id, "https://www.hangshare.com/user/{$data->id}/", [
-                                'target' => '_blank'
+                        'target' => '_blank'
                     ]);
                 },
-                    ],
-                    [
-                        'attribute' => 'name',
-                        'format' => 'raw',
-                        'value' => function ($data) {
-                            return Html::a($data->name, "https://www.hangshare.com/user/{$data->id}/", [
-                                        'target' => '_blank'
-                            ]);
-                        },
-                            ],
-                            'email:email',
-                            [
-                                'attribute' => 'total_amount',
-                                'format' => 'raw',
-                                'value' => function ($data) {
-                                    if (isset($data->userStats)) {
-                                        return $data->userStats->total_amount;
-                                    } else {
-                                        return 'Deleted';
-                                    }
-                                }
-                            ],
-                            [
-                                'attribute' => 'available_amount',
-                                'format' => 'raw',
-                                'value' => function ($data) {
-                                    if (isset($data->userStats)) {
-                                        return $data->userStats->available_amount;
-                                    } else {
-                                        return 'Deleted';
-                                    }
-                                }
-                            ],
-                            [
-                                'attribute' => 'cantake_amount',
-                                'format' => 'raw',
-                                'value' => function ($data) {
-                                    if (isset($data->userStats)) {
-                                        return $data->userStats->cantake_amount;
-                                    } else {
-                                        return 'Deleted';
-                                    }
-                                }
-                            ],
-                            [
-                                'attribute' => 'post_count',
-                                'format' => 'raw',
-                                'value' => function ($data) {
-                                    if (isset($data->userStats))
-                                        return $data->userStats->post_count;
-                                    return 'Deleted';
-                                }
-                            ],
-                            [
-                                'attribute' => 'post_total_views',
-                                'format' => 'raw',
-                                'value' => function ($data) {
-                                    if (isset($data->userStats))
-                                        return $data->userStats->post_total_views;
-                                    return 'Deleted';
-                                }
-                            ],
-                            [
-                                'attribute' => 'profile_views',
-                                'format' => 'raw',
-                                'value' => function ($data) {
-                                    if (isset($data->userStats))
-                                        return $data->userStats->profile_views;
-                                    return 'Deleted';
-                                }
-                            ],
-                            [
-                                'attribute' => 'gender',
-                                'format' => 'raw',
-                                'value' => function ($data) {
-                                    return $data->gender == 1 ? 'Male' : 'Female';
-                                },
-                                'filter' => Html::dropDownList('UserSearch[gender]', '', ['1' => 'Male', '2' => 'Female'], ['prompt' => ''])
-                            ],
-                            [
-                                'attribute' => 'plan',
-                                'format' => 'raw',
-                                'value' => function ($data) {
-                                    return $data->plan == 1 ? 'GOLD' : 'Normal';
-                                },
-                                'filter' => Html::dropDownList('UserSearch[plan]', '', ['1' => 'GOLD', '0' => 'Normal', '-1' => 'Not Completed'], ['prompt' => ''])
-                            ],
-                            [
-                                'attribute' => 'country',
-                                'format' => 'raw',
-                                'value' => function ($data) {
-                                    if ($data->country == 0 || !isset($data->location))
-                                        return '<i> غير محدد</i>';
-
-                                    return $data->location->name;
-                                },
-                                'filter' => Html::dropDownList('UserSearch[country]', '', ArrayHelper::map(Country:: find()->all(), 'id', 'name'), ['prompt' => ''])
-                            ],
-                            'created_at',
-                            [
-                                'class' => 'yii\grid\ActionColumn',
-                            //'template' => '{view}{de}'
-                            ],
-                        ],
+            ],
+            [
+                'attribute' => 'name',
+                'format' => 'raw',
+                'value' => function ($data) {
+                    return Html::a($data->name, "https://www.hangshare.com/user/{$data->id}/", [
+                        'target' => '_blank'
                     ]);
-                    ?>
+                },
+            ],
+            'email:email',
+            [
+                'attribute' => 'total_amount',
+                'format' => 'raw',
+                'value' => function ($data) {
+                    if (isset($data->userStats)) {
+                        return $data->userStats->total_amount;
+                    } else {
+                        return 'Deleted';
+                    }
+                }
+            ],
+            [
+                'attribute' => 'available_amount',
+                'format' => 'raw',
+                'value' => function ($data) {
+                    if (isset($data->userStats)) {
+                        return $data->userStats->available_amount;
+                    } else {
+                        return 'Deleted';
+                    }
+                }
+            ],
+            [
+                'attribute' => 'cantake_amount',
+                'format' => 'raw',
+                'value' => function ($data) {
+                    if (isset($data->userStats)) {
+                        return $data->userStats->cantake_amount;
+                    } else {
+                        return 'Deleted';
+                    }
+                }
+            ],
+            [
+                'attribute' => 'post_count',
+                'format' => 'raw',
+                'value' => function ($data) {
+                    if (isset($data->userStats))
+                        return $data->userStats->post_count;
+                    return 'Deleted';
+                }
+            ],
+            [
+                'attribute' => 'gender',
+                'format' => 'raw',
+                'value' => function ($data) {
+                    return $data->gender == 1 ? 'Male' : 'Female';
+                },
+                'filter' => Html::dropDownList('UserSearch[gender]', '', ['1' => 'Male', '2' => 'Female'], ['prompt' => ''])
+            ],
+            [
+                'attribute' => 'deleted',
+                'format' => 'raw',
+                'value' => function ($data) {
+                    return $data->plan == 1 ? 'Banded' : 'Active';
+                },
+                'filter' => Html::dropDownList('UserSearch[deleted]', '', ['1' => 'Banded', '0' => 'Active'], ['prompt' => ''])
+            ],
+            [
+                'attribute' => 'plan',
+                'format' => 'raw',
+                'value' => function ($data) {
+                    return $data->plan == 1 ? 'GOLD' : 'Normal';
+                },
+                'filter' => Html::dropDownList('UserSearch[plan]', '', ['1' => 'GOLD', '0' => 'Normal', '-1' => 'Not Completed'], ['prompt' => ''])
+            ],
+            [
+                'attribute' => 'country',
+                'format' => 'raw',
+                'value' => function ($data) {
+                    if ($data->country == 0 || !isset($data->location))
+                        return '<i> غير محدد</i>';
+
+                    return $data->location->name;
+                },
+                'filter' => Html::dropDownList('UserSearch[country]', '', ArrayHelper::map(Country:: find()->all(), 'id', 'name'), ['prompt' => ''])
+            ],
+            'created_at',
+            [
+                'class' => 'yii\grid\ActionColumn',
+                //'template' => '{view}{de}'
+            ],
+        ],
+    ]);
+    ?>
 </div>
