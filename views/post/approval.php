af60d99af84c95d1877604d6736eae7d7f9ab914
khaled <kaldmax@hotmail.com>
2016-08-28 07:28:15 -0600

    category

--- views/post/approval.php
+++ views/post/approval.php
@@ -21,7 +21,6 @@ $this->params['breadcrumbs'][] = $this->title;
         'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-            'id',
             [
                 'attribute' => 'cover',
                 'label' => 'Cover',
