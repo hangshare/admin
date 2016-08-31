7979c0f98e200d419751e6ccca145c6fbe8e597a
khaled <kaldmax@hotmail.com>
2016-08-30 18:46:02 -0600

    category

--- components/Imageresize.php
+++ components/Imageresize.php
@@ -14,9 +14,34 @@ class Imageresize extends Component
     private $height = 200;
     private $method = 'resize';
     private $mediaFile = '/web/media';
+    private $quality = 75;
+    private $s3;
+    private $post_sizes = [
+        ['width' => '150', 'height' => '100', 'method' => 'crop'],
+        ['width' => '1000', 'height' => '1000', 'method' => 'resize'],
+        ['width' => '500', 'height' => '500', 'method' => 'resize'],
+        ['width' => '300', 'height' => '250', 'method' => 'crop'],
+        ['width' => '400', 'height' => '230', 'method' => 'crop'],
+        ['width' => '400', 'height' => '250', 'method' => 'crop'],
+        ['width' => '400', 'height' => '290', 'method' => 'crop'],
+        ['width' => '300', 'height' => '290', 'method' => 'crop'],
+        ['width' => '500', 'height' => '350', 'method' => 'resize'],
+        ['width' => '500', 'height' => '350', 'method' => 'crop'],
+        ['width' => '900', 'height' => '430', 'method' => 'crop'],
+    ];
+    private $user_sizes = [
+        ['width' => '25', 'height' => '25', 'method' => 'crop'],
+        ['width' => '50', 'height' => '50', 'method' => 'crop'],
+        ['width' => '80', 'height' => '80', 'method' => 'crop'],
+        ['width' => '100', 'height' => '80', 'method' => 'crop'],
+        ['width' => '300', 'height' => '300', 'method' => 'crop'],
+        ['width' => '300', 'height' => '250', 'method' => 'crop'],
+    ];
+
 
     public function __construct($config = array())
     {
+        $this->s3 = Yii::$app->customs3;
         $this->mediaFile = Yii::$app->basePath . $this->mediaFile;
     }
 
@@ -42,6 +67,71 @@ class Imageresize extends Component
     {
         $json = json_decode($string);
         return (json_last_error() == JSON_ERROR_NONE) ? $json : false;
+    }
+
+
+    public function s3Resize($path, $width, $height, $method)
+    {
+        $filethump = $this->thumpName($width, $height, $method);
+        $dir = dirname($path);
+        $thumppath = $dir . '/' . $filethump;
+        $key_folder = basename($dir);
+        $ext = pathinfo($path, PATHINFO_EXTENSION);
+        if (!is_dir($thumppath)) {
+            mkdir($thumppath, 0777, true);
+        }
+        $file_name = basename($path);
+        $thumppath = $thumppath . '/' . $file_name;
+
+        $im = new \Imagick($path);
+        $im->setImageCompression(\Imagick::COMPRESSION_LOSSLESSJPEG);
+        $im->setImageFormat($ext);
+        $im->setImageCompressionQuality($this->quality);
+        $im->stripImage();
+
+        if ($method == 'resize') {
+            $im->resizeImage($width, $height, \Imagick::FILTER_LANCZOS, 0.9, true);
+            $im->writeImage($thumppath);
+        } else {
+            if ($im->getimageheight() > $im->getimagewidth() || (($width + $height) > ($im->getimagewidth() + $im->getimageheight()))) {
+                $im = new \Imagick($path);
+                $im->scaleimage(0, $height);
+                $im->writeimage($thumppath);
+                $bkim = new \Imagick($path);
+                $bkim->evaluateImage(\Imagick::EVALUATE_MULTIPLY, 0.3, \Imagick::CHANNEL_ALPHA);
+                $bkim->cropThumbnailImage($width, $height);
+                $bkim->blurimage(25, 15);
+                $bkim->setImageFormat('png');
+                $bkim->setImageCompression(\Imagick::COMPRESSION_LOSSLESSJPEG);
+                $im->resizeImage($width / 1.5, $height * 2, \Imagick::FILTER_LANCZOS, 0.9, true);
+                $shadow = $im->clone();
+                $shadow->setImageBackgroundColor(new \ImagickPixel('black'));
+                $shadow->shadowImage(70, 10, 15, 15);
+                $bkim->compositeImage($shadow, \Imagick::COMPOSITE_OVER, (((($bkim->getImageWidth()) - ($im->getImageWidth()))) / 2), (((($bkim->getImageHeight()) - ($im->getImageHeight()))) / 2));
+                $bkim->compositeImage($im, \Imagick::COMPOSITE_DEFAULT, (((($bkim->getImageWidth()) - ($im->getImageWidth()))) / 2), (((($bkim->getImageHeight()) - ($im->getImageHeight()))) / 2));
+                @$bkim->writeImage($thumppath);
+            } else {
+                $im->cropThumbnailImage($width, $height);
+                $im->writeImage($thumppath);
+            }
+        }
+        $im->clear();
+        $im->destroy();
+
+        $this->s3->uploadFromPath($thumppath, 'hangshare-media', "{$key_folder}/{$filethump}/{$file_name}");
+        @unlink($thumppath);
+        return $this;
+    }
+
+    public function PatchResize($bucket, $key, $type = 'post')
+    {
+        $path = $this->s3->downloadFile($bucket, $key);
+        $array = "{$type}_sizes";
+        foreach ($this->$array as $size) {
+            $this->s3Resize($path, $size['width'], $size['height'], $size['method']);
+        }
+        @unlink($path);
+        return true;
     }
 
     protected function thumpName($width, $height, $method)
