60c05efcf08c875c7daf32b746d3e51040758322
khaled <kaldmax@hotmail.com>
2016-11-14 18:46:20 +0200

    category

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -35,6 +35,7 @@ AppAsset::register($this);
                 ['label' => 'Users', 'url' => ['/user/index']],
                 ['label' => 'Transaction', 'url' => ['/usertransactions/index']],
                 ['label' => 'Posts', 'url' => ['/post/index']],
+                ['label' => 'Comments', 'url' => ['/postcomments/index']],
                 ['label' => 'Approval', 'url' => ['/post/approval']],
                 ['label' => 'Bulk Email', 'url' => ['/newsletter/index']],
 //                ['label' => 'Views', 'url' => ['/postview/index']],
